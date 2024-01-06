9ce8c223bf82bcc932275db46ec4ac13f7376653
Kostia <231768@student.uek.krakow.pl>
2024-01-06 22:40:34 +0100

    Small changes

--- backend/mockaroo/mockaroo.js
+++ backend/mockaroo/mockaroo.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv')
 dotenv.config()
 
 const mockarooURL = "https://api.mockaroo.com/api/generate.json?key="
-const mocakrooApiKey = "ea0440b0";
+const mocakrooApiKey = "7a1b58b0"; // ea0440b0
 
 async function getArtistsAndTracksList() {
     const response = await fetch(`${mockarooURL}${mocakrooApiKey}&array=true&schema=team-project-schema`, {
@@ -17,6 +17,7 @@ async function getArtistsAndTracksList() {
             const newElement = {
                 name: splitted[0],
                 song: splitted[1],
+                price: responseJson[i].price,
             }
             responseJson[i] = newElement;
         }
