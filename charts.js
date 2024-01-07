144e307ca794f279f270329fc864c8a2fc91f2e9
Kostia <231768@student.uek.krakow.pl>
2024-01-07 22:43:10 +0100

    Advanced charts

--- charts.js
+++ charts.js
@@ -1,21 +1,10 @@
 async function loadArtistList() {
     const responce = await fetch("https://vinyl-store-olive.vercel.app/getArtistsAndNames");
     const data = await responce.json();
-    return data.slice(0, 7);
+    return data;
 }
 
-async function getPriceChartData(data) {
-    const chart_data = []
-    for (const element of data){
-        chart_data.push({
-            song: element.song,
-            price: element.price,
-        })
-    }
-    return chart_data;
-}
-
-async function createChart(data){
+async function createFirstChart(data){
     new Chart(
         document.getElementById('price_chart'),
         {
@@ -24,8 +13,29 @@ async function createChart(data){
             labels: data.map(row => row.song),
             datasets: [
               {
-                label: 'Price',
+                label: 'Price ($)',
                 data: data.map(row => row.price)
+              }
+            ]
+          }
+        }
+      );
+}
+
+async function createSecondChart(data){
+    new Chart(
+        document.getElementById('some_chart'),
+        {
+          type: 'line',
+          data: {
+            labels: data.map(row => row.song),
+            datasets: [
+              {
+                label: 'Price ($)',
+                data: data.map(row => row.price),
+                borderColor: '#8c43d2',
+                borderWidth: 2,
+                fill: true,
               }
             ]
           }
@@ -35,8 +45,8 @@ async function createChart(data){
 
 async function render() {
     const data = await loadArtistList(); // no images
-    const price_chart_data = getPriceChartData(data);
-    await createChart(data);
+    await createFirstChart(data.slice(0, 7));
+    await createSecondChart(data.slice(8, 15));
 }
 
 render();
